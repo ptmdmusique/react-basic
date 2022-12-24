363bd9167f2aa0ca88fa1e7274c42c58605a8ab1
Phuong <thphuongster@gmail.com>
2022-12-24 10:03:38 -0800

    Commit Render Static List

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import { sampleList } from './sampleList';
+
+function ListDisplay() {
+  let items: JSX.Element[] = [];
+  for (let i = 0; i < sampleList.length; i++) {
+    let displayName = sampleList[i].name;
+    let item =
+      <div key={i}>
+        <span>
+          {displayName}
+        </span>
+      </div>
+
+    items.push(item);
+  }
+  
+  return <div>{items}</div>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ListDisplay />
   );
 }
 
