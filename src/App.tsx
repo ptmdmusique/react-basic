370d37d2ff44b4b6c58a9467e2805b5943524641
Duc Phan <ptmdmusic@gmail.com>
2022-12-23 18:57:24 -0800

    add basic design - #4

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,19 @@ import { todoList } from "./data";
 
 function App() {
   return (
-    <div>
-      {todoList.map((todo) => (
-        <p>{todo.name}</p>
-      ))}
+    <div className="app">
+      <h1 className="title">
+        <b>My Fancy TODO App</b>
+      </h1>
+
+      <div className="todo-container">
+        {todoList.map((todo) => (
+          <span className="task" key={todo.id}>
+            <p className="id">{todo.id}</p>
+            <p className="task-name">{todo.name}</p>
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
