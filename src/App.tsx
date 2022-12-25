8a63033048785372d75ba4a3890ca8bc17875ff9
Phuong <thphuongster@gmail.com>
2022-12-24 17:40:12 -0800

    start working on figma design

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,27 @@ import React from 'react';
 import './App.css';
 
 import { sampleList } from './sampleList';
+  
 
 function ListDisplay() {
   let items: JSX.Element[] = [];
   for (let i = 0; i < sampleList.length; i++) {
     let displayName = sampleList[i].name;
     let item =
-      <div key={i}>
-        <span>
-          {displayName}
-        </span>
-      </div>
-
+    <div key={i} >
+      <div className='row'>
+          <div className = 'col1'>
+            <div className='box1'>
+              { sampleList[i].id } 
+            </div>
+          </div>
+          <div className = 'col2'>
+            <div className='box2'>
+              { displayName} 
+            </div>
+          </div>
+        </div>
+    </div>  
     items.push(item);
   }
   
