d5b44dcace9707baf277dbbaf093004f605fb602
Phuong <thphuongster@gmail.com>
2022-12-29 17:57:12 -0800

    Feature 4 reviewed

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { taskList } from "./taskList";
 function TaskShow() {
   return (
     <div>
+      <div className="title">
+        <h2>My Fancy TODO App</h2>
+      </div>
       {taskList.map((task) => (
         <div className="container">
             <div className="id-container">{task.id}</div>
