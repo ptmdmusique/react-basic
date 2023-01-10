6d77585610109adec2185aef5a64176d52b1fb72
Phuong <thphuongster@gmail.com>
2023-01-09 21:00:00 -0800

    feature 6 todo 4th commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,97 +1,85 @@
 import "./App.css";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { taskList } from "./taskList";
 
-import icon from './icons8-delete.svg';
-
+import icon from "./icons8-delete.svg";
 
 // Create a list with additional field to mark deleted task
-interface typeExtended {
+interface TypeExtended {
   id: number;
   name: string;
   isDeleted: boolean;
 }
 
-let taskListExt: typeExtended[] = [] ;
+let taskListExt: TypeExtended[] = [];
 
-taskList.map( task => (
-    taskListExt.push(
-      {
-        'id'       : task.id,
-        'name'     : task.name,
-        'isDeleted'  : false 
-      }
-    ))
-)
 //
 function TaskShow() {
-  const [tasks, setTasks ] = useState(taskListExt);
-  const [newTaskName, setNewTaskName] = useState('');
-  
-  const handleSubmit:React.FormEventHandler<HTMLFormElement> = (event) => {
-    
+  const [tasks, setTasks] = useState(
+    taskList.map((task) => ({
+      id: task.id,
+      name: task.name,
+      isDeleted: false,
+    }))
+  );
+  const [newTaskName, setNewTaskName] = useState("");
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
-    const newId  = tasks[ tasks.length-1 ].id + 1;
-    
-      setTasks( (previousTask) => [
-                  ...previousTask,
-                  { id: newId,
-                    name: newTaskName,
-                    isDeleted: false,
-                  }
-              ]
-            )
-  }
+    const newId = tasks[tasks.length - 1].id + 1;
+
+    setTasks((previousTask) => [
+      ...previousTask,
+      { id: newId, name: newTaskName, isDeleted: false },
+    ]);
+  };
 
   return (
-
     <div className="App">
-
       <h2>My Fancy TODO App</h2>
       {tasks.map((task) => (
         <div className="container" key={task.id}>
-            <div className="id-container" >
-              <span className="text">{task.id}</span>
-            </div>
-            <div className="task-name-container">
-              <span className="text">{task.name}</span>
-            </div>
+          <div className="id-container">
+            <span className="text">{task.id}</span>
+          </div>
+          <div className="task-name-container">
+            <span className="text">{task.name}</span>
+          </div>
 
-            <div>
-              <button 
-                className="delete-button"
-                type='button'
-                onClick={ () => {
-                  task.isDeleted = true
-                  setTasks(tasks.filter( item => ! item.isDeleted))
-                }}  
-              >
-                <img className='delete-button-icon' src={icon} width='30px' height='30px' />
-              </button>
-            </div>  
+          <div>
+            <button
+              className="delete-button"
+              type="button"
+              onClick={() => {
+                task.isDeleted = true;
+                setTasks(tasks.filter((item) => !item.isDeleted));
+              }}
+            >
+              <img className="delete-button-icon" src={icon} />
+            </button>
+          </div>
         </div>
       ))}
 
-      <div className="new-task-container"> 
-        <form onSubmit={handleSubmit} >
-          <div>
-            <label for='taskname'>Task name</label><br />
-               <input 
-                  name='taskname'
-                  className='new-task-name'
-                  type='text'
-                  placeholder="New task name here"
-                  onChange={ event => setNewTaskName(event.target.value)}
-                  value={newTaskName}
-                />
-                <button type='submit' className="new-task-button">
-                    <font color='white'>Add new</font>
-                </button>
-          </div>
-        </form>
-      </div>  
+      <form onSubmit={handleSubmit} className="new-task-container">
+        <div className="my-input">
+          <label htmlFor="taskname">Task name</label>
+          <input
+            name="taskname"
+            className="new-task-name"
+            type="text"
+            placeholder="New task name here"
+            onChange={(event) => setNewTaskName(event.target.value)}
+            value={newTaskName}
+          />
+        </div>
+
+        <button type="submit" className="new-task-button">
+          Add new
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
 function App() {
